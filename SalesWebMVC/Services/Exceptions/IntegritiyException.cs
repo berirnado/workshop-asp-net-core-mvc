9a0d02fcb9c28cfa2fb4bbcffabd5698eda1640b
Bernardo <be,zito@hotmail.com>
2023-02-24 15:58:46 -0300

    Exception handling for delete

---
+++ SalesWebMVC/Services/Exceptions/IntegritiyException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SalesWebMvc.Services.Exceptions
+{
+	public class IntegritiyException : ApplicationException
+	{
+		public IntegritiyException(string message) : base(message) { }
+	}
+}
--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -36,9 +36,16 @@ namespace SalesWebMvc.Services
 
 		public async Task RemoveAsync(int id)
 		{
-			var obj = await _context.Seller.FindAsync(id);
-			_context.Seller.Remove(obj);
-			_context.SaveChanges();
+			try
+			{
+				var obj = await _context.Seller.FindAsync(id);
+				_context.Seller.Remove(obj);
+				await _context.SaveChangesAsync();
+			}
+			catch (DbUpdateException e)
+			{
+				throw new IntegritiyException("Can't delete seller because he/she has sales");
+			}
 		}
 
 		public async Task UpdateAsync(Seller obj)
