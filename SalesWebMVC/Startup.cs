fe2e221aeb332997c6e02b62fe3018695816224a
Bernardo <be,zito@hotmail.com>
2023-02-27 09:14:33 -0300

    Implementing simple search

--- SalesWebMVC/Startup.cs
+++ SalesWebMVC/Startup.cs
@@ -46,7 +46,8 @@ namespace SalesWebMVC
             services.AddScoped<SeedingService>();
             services.AddScoped<SellerService>();
 			services.AddScoped<DepartmentService>();
-		}
+            services.AddScoped<SalesRecordService>();
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
